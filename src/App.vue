de2fff4a66ee0837619cfef72204835e12d17139
Justin Tavares <justintavares@gmail.com>
2020-01-16 00:07:23 -0500

    match existing site

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,19 @@
     <v-content>
       <Homepage />
     </v-content>
+     <v-footer
+        app
+        absolute
+        padless
+				class="font-weight-medium"
+			>
+				<v-col
+				class="text-center"
+				cols="12"
+				>
+				{{ new Date().getFullYear() }} — <strong>Built with Vue/Vuetify</strong>
+				</v-col>
+			</v-footer>
   </v-app>
 </template>
 
--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,16 +1,32 @@
 <template>
-<div id="homepage">
-
-
-</div>
+  <div id="homepage">
+    <v-parallax src="../assets/pic.jpg">
+      <v-layout column justify-center align-center>
+        <v-flex xs12 sm8 md6>
+          <v-card color="rgb(255, 255, 255, 0.8)">
+            <v-card-title class="custom-selector headline">
+              <h1 class="display-1 font-weight-thin mb-4">Justin Tavares</h1>
+              <br />
+            </v-card-title>
+            <v-card-subtitle>
+              <h3 class="text-center">
+                <a href="resume.html">Résumé</a> ||
+                <a href="https://www.linkedin.com/in/justin-tavares-b0459a5a/"
+                  >Linkedin</a
+                >
+              </h3>
+            </v-card-subtitle>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-parallax>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'homepage',
+  name: "homepage",
 
-  data: () => ({
-    
-  }),
+  data: () => ({})
 };
 </script>
