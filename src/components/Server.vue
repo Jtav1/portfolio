2260b685d8eb4650a6ace98833231dcb77957216
Justin Tavares <justintavares@gmail.com>
2020-08-06 21:47:29 -0400

    fix wording

--- src/components/Server.vue
+++ src/components/Server.vue
@@ -2,7 +2,7 @@
   <div id="portfolio" class="server">
       <a id="server"></a>
     <span style="text-align:center;">
-      <h2>How does this website work?</h2>
+      <h2>How was this website built?</h2>
     </span>
     <div class="leftHalf"> </div>
     <div class="rightHalf">
@@ -10,11 +10,12 @@
         <v-list-item three-line>
           <v-list-item-content>
             <div class="overline mb-4">My Homelab</div>
-            <v-list-item-title class="headline mb-1">Unraid on bare metal</v-list-item-title>
-            <div class="text--primary, cardText">My homelab currently contains a Dell R710 running Unraid 6.8.1</div>
-            <div class="text--primary, cardText">This server runs multiple docker containers and linux VMs, and one Linux Mint workstation</div>
+            <v-list-item-title class="headline mb-1">Unraid on Bare Metal</v-list-item-title>
+            <div class="text--primary, cardText">My homelab currently contains a Dell R710 running Unraid 6.8.1.</div>
+            <div class="text--primary, cardText">This server runs multiple docker containers and linux VMs, and one Linux Mint workstation.</div>
             <div class="text--primary, cardText">Future projects for this server may include a smart home server. Currently, the server runs a nextcloud instance, a reverse nginx proxy to enable SSL-enabled connections to websites hosted there, and webhosting for this site.</div>
-            <div class="text--primary, cardText">This website runs in a docker container on an Ubuntu VM behind a reverse proxy that provides an SSL certificate</div>
+            <div class="text--primary, cardText">This website runs in a docker container on an Ubuntu VM behind a reverse proxy that provides an SSL certificate.</div>
+            <div class="text--primary, cardText">This website was built with Vuejs using components from Vuetify, leveraging babel and webpack.</div>
           </v-list-item-content>
         </v-list-item>
       </v-card>
